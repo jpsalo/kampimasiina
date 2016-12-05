34fa375fa6a5588e42166ec8c287e20aa46e733f
jp <jpsalo@zoho.com>
2016-12-05 16:10:52 +0200

    Beginnings of the UI module.

--- Kampi_Main.py
+++ Kampi_Main.py
@@ -9,13 +9,15 @@ Created on Sun Nov  6 23:02:40 2016
 from random import randint
 import subprocess
 
+subprocess.call(['python', 'kampi_ui.py'])
+
 koenumero = 1
 
-while (koenumero < 3):
-    koenumero = randint(0,2)
-    if (koenumero == 0):
-        subprocess.call(['python', 'KampiPos.py'])
-    if (koenumero == 1):
-        subprocess.call(['python', 'KampiNeg.py'])
-    if (koenumero == 2):
-        subprocess.call(['python', 'KampiNeutr.py'])
+# while (koenumero < 3):
+#     koenumero = randint(0, 2)
+#     if (koenumero == 0):
+#         subprocess.call(['python', 'KampiPos.py'])
+#     if (koenumero == 1):
+#         subprocess.call(['python', 'KampiNeg.py'])
+#     if (koenumero == 2):
+#         subprocess.call(['python', 'KampiNeutr.py'])
