b96909ecd562df3eeafff0eb9ca8cef26a1b52b8
jp <jpsalo@zoho.com>
2016-12-06 11:35:22 +0200

    Added a placeholder for background questions page.

--- kampi_ui.py
+++ kampi_ui.py
@@ -2,6 +2,7 @@
 
 import config
 import landing
+import background_questions
 import experiment
 
 pages = []
@@ -17,9 +18,19 @@ def activate_landing():
     landing_data = landing.generate_page(root,
                                          window_w,
                                          window_h,
-                                         activate_experiment)
+                                         activate_background_questions)
     landing_data.pack(padx=20, pady=40)
     pages.append(landing_data)
+
+
+def activate_background_questions():
+    background_questions_data = background_questions.generate_page(root,
+                                                                   window_w,
+                                                                   window_h,
+                                                                   activate_experiment)
+    forget_other_pages(background_questions_data)
+    background_questions_data.pack(padx=20, pady=40)
+    pages.append(background_questions_data)
 
 
 def activate_experiment():
@@ -30,6 +41,7 @@ def activate_experiment():
     forget_other_pages(experiment_data)
     experiment_data.pack(padx=20, pady=40)
     experiment.refresh_page(root, activate_third_page)
+    pages.append(experiment_data)
 
 
 def activate_third_page():
@@ -47,8 +59,6 @@ root.overrideredirect(1)
 root.geometry("%dx%d+0+0" % (window_w, window_h))
 root.configure(background=config.background_color)
 
-
-# Create pages
 
 activate_landing()
 
