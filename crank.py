2596e32d6a7f2fa72f82a754b7c1b9c3a6cea0cd
JP Salo <jukapeka@gmail.com>
2016-12-14 17:33:14 +0200

    Switched from mock crank to the real one.

--- crank.py
+++ crank.py
@@ -2,19 +2,20 @@ import serial
 
 ser = serial.Serial()
 ser.baudrate = 9600
-# ser.port = '/dev/tty.usbmodem1411'  # NOTE: Change this to COM1
+# ser.port = '/dev/tty.usbmodem1411'
+ser.port = 'COM1'
 
 
-signal_strength = [0] * 110
-signal_strength.append(1)
-counter = 0
+# signal_strength = [0] * 110
+# signal_strength.append(1)
+# counter = 0
 
 
 def read_serial():
-    # ser.open()
-    # value = float(ser.readline().strip())
-    # ser.close()
-    global counter
-    value = signal_strength[counter]
-    counter = 0 if counter == len(signal_strength) - 1 else counter + 1
+    ser.open()
+    value = float(ser.readline().strip())
+    ser.close()
+    # global counter
+    # value = signal_strength[counter]
+    # counter = 0 if counter == len(signal_strength) - 1 else counter + 1
     return value
