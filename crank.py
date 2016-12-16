802ad6e1bfe347fff8a06027712a032740854a06
jp <jpsalo@zoho.com>
2016-12-16 12:19:21 +0200

    Use correct serial port for the test machine.

--- crank.py
+++ crank.py
@@ -2,8 +2,8 @@ import serial
 
 ser = serial.Serial()
 ser.baudrate = 9600
-# ser.port = '/dev/tty.usbmodem1411'
-ser.port = 'COM1'
+# ser.port = '/dev/tty.usbmodem1411'    # For MacBook
+ser.port = 'COM4'
 
 
 # signal_strength = [0] * 110
@@ -15,7 +15,7 @@ def read_serial():
     ser.open()
     value = float(ser.readline().strip())
     ser.close()
-    # global counter
-    # value = signal_strength[counter]
-    # counter = 0 if counter == len(signal_strength) - 1 else counter + 1
+#    global counter
+#    value = signal_strength[counter]
+#    counter = 0 if counter == len(signal_strength) - 1 else counter + 1
     return value
