93513e4a77f0a47665b7e3a4bdaa5dcb82032d1a
jpsalo <jpsalo@zoho.com>
2017-12-22 17:09:50 +0200

    Commenting

--- web/questionnaires_csv.sh
+++ web/questionnaires_csv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# https://stackoverflow.com/a/39534852/7010222
 heroku run python manage.py dumpdata motivation --natural-foreign -- | jq '[.[] | {mturk: .fields."mturk", experiment_type: .fields."experiment_type", earnings: .fields."earnings"}]' > questionnaires.json
 json2csv -i questionnaires.json > questionnaires.csv
