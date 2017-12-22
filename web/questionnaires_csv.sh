d3868ba7779cf16c19d2a5dd5fba0d90860d0110
jpsalo <jukapeka@gmail.com>
2017-12-22 17:31:16 +0200

    Updated questionnaire csv saving tool to use local json to csv converter

--- web/questionnaires_csv.sh
+++ web/questionnaires_csv.sh
@@ -2,4 +2,4 @@
 
 # https://stackoverflow.com/a/39534852/7010222
 heroku run python manage.py dumpdata motivation --natural-foreign -- | jq '[.[] | {mturk: .fields."mturk", experiment_type: .fields."experiment_type", earnings: .fields."earnings"}]' > questionnaires.json
-json2csv -i questionnaires.json > questionnaires.csv
+./node_modules/.bin/json2csv -i questionnaires.json > questionnaires.csv
