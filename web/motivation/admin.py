d3bd21ee1a686a20399c62e15906300c400e734e
jps <jpsalo@zoho.com>
2019-05-09 15:49:25 +0300

    Added questionnaire export to Django admin

--- web/motivation/admin.py
+++ web/motivation/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
+from import_export import resources
 from .models import Questionnaire
+from import_export.admin import ImportExportModelAdmin
+
+
+class QuestionnaireResource(resources.ModelResource):
+
+    class Meta:
+        model = Questionnaire
+
+
+class QuestionnaireAdmin(ImportExportModelAdmin):
+    resource_class = QuestionnaireResource
+
 
 # Register your models here.
 
-admin.site.register(Questionnaire)
+admin.site.register(Questionnaire, QuestionnaireAdmin)
