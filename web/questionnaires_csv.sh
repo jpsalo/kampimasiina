03529731e7a0186b4bd5393a7a9ce81c9bb02f2f
jps <jpsalo@zoho.com>
2019-05-09 15:48:28 +0300

    Updated csv export script

--- web/questionnaires_csv.sh
+++ web/questionnaires_csv.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Dump data into csv
 # https://stackoverflow.com/a/39534852/7010222
-heroku run python manage.py dumpdata motivation --natural-foreign -- | jq '[.[] | {mturk: .fields."mturk", experiment_type: .fields."experiment_type", earnings: .fields."earnings"}]' > questionnaires.json
+# https://stackoverflow.com/questions/39457549/heroku-django-dumpdata/39534852#comment81138914_39534852
+heroku run --app kampi python manage.py dumpdata motivation --natural-foreign -- | jq '[.[] | {mturk: .fields."mturk", experiment_type: .fields."experiment_type", earnings: .fields."earnings"}]' > questionnaires.json
 ./node_modules/.bin/json2csv -i questionnaires.json > questionnaires.csv
